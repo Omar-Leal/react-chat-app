b31c72ec6ff693e80ee9645feec7eb9214fd837a
OmarLeal2503 <omarenriquelealr@gmail.com>
2021-03-02 22:05:14 -0400

    App finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './styles/App.css'
 
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
     return (
       <ChatEngine 
         height="100vh"
-        projectID="
-        ad454e1a-5787-460e-94d7-27cf99cc4726"
-        userName="omar_leal"        
-        userSecret="123123"
+        projectID="ad454e1a-5787-460e-94d7-27cf99cc4726"
+        userName={localStorage.getItem('username')}       
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} /> }
       
       />
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -5,7 +5,7 @@ const TheirMessage = ({ lastMessage, message }) =>  {
      <div className="message-row">
           {isFirstMessageByUser && (
             <div className="message-avatar"
-            style={{backgroundImage: `url(${message?.sender?.avatar})` }}
+            style={{backgroundImage: message.sender && `url(${message?.sender?.avatar})` }}
             />
                        
           )}
